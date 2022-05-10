cd5520cebcc08c0e580960fe842177126d6fc001
seba <seba.rodriguezm@duocuc.cl>
2022-05-09 23:24:08 -0400

    asdas

--- js/formulario_jquery.js
+++ js/formulario_jquery.js
@@ -30,3 +30,51 @@ $("#guardar").click(function() {
 
 
 })
+
+
+$("#formulario2").validate({
+    rules: {
+        nombre: {
+            required: true,
+            minlength: 5,
+            maxlength: 200
+        },
+        apellido: {
+            required: true,
+            minlength: 5,
+            maxlength: 200
+        },
+        usuario: {
+            required: true,
+            minlength: 5,
+            maxlength: 200
+        },
+        pass: {
+            required: true,
+            minlength: 5,
+            maxlength: 200
+        },
+        email: {
+            required: true,
+            email: true
+        },
+    }
+})
+
+
+
+
+$("#guardar2").click(function() {
+
+    if ($("#formulario2").valid() == false) {
+        return;
+    }
+    let nombre = $("#nombre").val()
+    let apellido = $("#apellido").val()
+    let usuario = $("#usuario").val()
+    let pass = $("#contrasenia").val()
+    let email = $("#correo").val()
+    alert("registro creado con éxito")
+
+
+})
