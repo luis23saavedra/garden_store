28d6d3cda5a0eafae94399d69749fdb7a6c967ac
seba <seba.rodriguezm@duocuc.cl>
2022-06-03 14:01:39 -0400

    hfgf

--- store/forms.py
+++ store/forms.py
@@ -2,6 +2,7 @@ from dataclasses import fields
 from django import forms
 from .models import Cliente, Producto
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ProductoForm(forms.ModelForm):
     
@@ -18,4 +19,7 @@ class ClienteForm(forms.ModelForm):
         
 ##se importa usercreationform y genera automaticamente el formulaio para registrar un usuario y asi no hay que crear superuser         
 class CustomerUserCreationForm(UserCreationForm):  
-    pass      
+    
+    class Meta:
+        model = User
+        fields = ['username', "email", "password1", "password2"]
