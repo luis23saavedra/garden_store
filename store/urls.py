f9e049d86179cf990ad01fd8cbfad6d3d05b4dbc
sebarm <89598752+sebarm@users.noreply.github.com>
2022-06-06 13:14:39 -0400

    Add files via upload

---
+++ store/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('producto', views.ProductoViewSet)
+
+urlpatterns = [
+     path('', views.store, name="store"),
+     path('cart/', views.cart, name="cart"),
+     path('checkout/', views.checkout, name="checkout"),
+     path('agregar_producto/', views.agregar_producto, name="agregar_producto"),
+     path('listar/', views.listar, name="listar"),
+     path('editar_producto/<id>/', views.editar_producto, name="editar_producto"),
+     path('eliminar_producto/<id>/', views.eliminar_producto, name="eliminar_producto"),
+     path('listar_cliente/', views.listar_cliente, name="listar_cliente"),
+     path('agregar_cliente/', views.agregar_cliente, name="agregar_cliente"),
+     path('registro/', views.registro, name="registro"),
+     path('api/', include(router.urls)),
+     
+]
