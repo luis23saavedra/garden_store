8205733e9bc0617aeeff5e488a3a71f49454e346
sebarm <89598752+sebarm@users.noreply.github.com>
2022-06-06 20:28:39 -0400

    Update serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -6,3 +6,8 @@ class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = '__all__'
+        
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'                
