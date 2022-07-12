0f789755b742b5689729aada0e830a02cc86ae9f
sebarm <89598752+sebarm@users.noreply.github.com>
2022-07-12 18:15:16 -0400

    Delete carro directory

--- carro/context_processor.py
+++
@@ -1,8 +0,0 @@
-def importe_total_carro(request):
-    total= 0
-    if(request.user.is_authenticated):
-        if 'carro' in request.session:
-            for key, value in request.session["carro"].items():
-                total=total +int(value["precio"])
-            
-    return {"importe_total_carro":total}
